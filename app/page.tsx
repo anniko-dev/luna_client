069a098a1eb81115016e9ee4a76487d4038dd772
Андрей Николаев <annikodev@gmail.com>
2024-01-06 18:53:47 +0300

    basic

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import styles from './page.module.css'
+import React from 'react'
 
-export default function Home() {
-  return <main className={styles.main} />
-}
+const Page = () => <div>body</div>
+
+export default Page
---
+++ src/components/root/RootHeader/RootHeader.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const RootHeader = () => (
+  <header>
+    <ul>
+      <li>link1</li>
+      <li>link2</li>
+      <li>link3</li>
+      <li>link4</li>
+    </ul>
+  </header>
+)
+
+export default RootHeader
